78233754478ab5bc675126fb88ccea613153915f
Saikat <iamsaikat4@gmail.com>
2023-02-09 17:16:16 +0530

    Add multiply and divide symbols

--- js-file.js
+++ js-file.js
@@ -13,8 +13,8 @@ function divide(a,b) {
 function operate(operator,a,b) {
     if (operator == '+') return add(a,b);
     if (operator == '-') return subtract(a,b);
-    if (operator == '*') return multiply(a,b);
-    if (operator == '/') return divide(a,b);
+    if (operator == '×') return multiply(a,b);
+    if (operator == '÷') return divide(a,b);
 };
 const display = document.querySelector('.display');
 const displayValue = document.querySelector('.display-value')
